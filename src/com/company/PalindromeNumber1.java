7faa41705946212c10d4f673d1f56cf9276cee1f
roshni090496 <rs09041001@gmail.com>
2022-04-18 22:47:23 -0700

    palindrome

--- src/com/company/PalindromeNumber1.java
+++ src/com/company/PalindromeNumber1.java
@@ -1,5 +1,24 @@
 package com.company;
-
+import java.util.Scanner;
 public class PalindromeNumber1 {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner (System.in);
+        System.out.println("Enter a number : ");
+        int n = in.nextInt();
+        int t = n;
+        int rev = 0;
+        while(t!=0) {
+            int d = t % 10;
+            rev = 10 * rev + d;
+            t = t / 10;
+        }
+        if (rev == n)
+            System.out.println( "Input n="+n+" then after reverse output is same "+rev+" then it is a palindrome");
+        else
+            System.out.print("Input n="+n+" then after reverse output is not same it's "+rev+" then it is not a palindrome number");
 
-}
+        }
+    }
+
+
